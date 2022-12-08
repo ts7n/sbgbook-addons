bc84477c609118efa5b93cdf69b6e491fc1800ec
Teddy <65257768+ts7n@users.noreply.github.com>
2022-12-08 15:33:00 -0600

    Update inject.js

--- inject.js
+++ inject.js
@@ -5,7 +5,8 @@ const config = {
             name: '',
             objective: '',
         }
-    }
+    },
+    lastCannotRA: 7
 }
 
 if(document.domain.includes('sbgbook.xyz') && document.URL.includes('student') && !document.ran) {
@@ -21,7 +22,7 @@ if(document.domain.includes('sbgbook.xyz') && document.URL.includes('student') &
             a.style.display = 'block';
             a.style.textAlign = 'center';
             document.querySelectorAll('th')[idx].innerHTML.split('\n')[0];
-            if(scorecards.length - 7 <= idx) return;
+            if(scorecards.length - config.lastCannotRA <= idx) return;
             let inner = document.createElement('a');
             inner.innerHTML = 'RA';
             inner.href = `${config.form.url}?entry.${config.form.fields.name}=${document.querySelectorAll('.scorehead')[0].innerHTML.split('>')[2].split('<')[0]}&entry.${config.form.fields.objective}=${document.querySelectorAll('th')[idx].innerHTML.split('\n')[0]}`;
