069950ba016299e3eb670449209d4b9ad94a8e0d
Teddy <65257768+ts7n@users.noreply.github.com>
2023-09-12 11:01:47 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -9,24 +9,29 @@ const config = {
     lastCannotRA: 7
 }
 
-if(document.domain.includes('sbgbook.xyz') && document.URL.includes('student') && !document.ran) {
-    document.ran = true;
-    setTimeout(() => {
+if(document.domain.includes('sbgbook.xyz') && document.URL.includes('student')) {
+    const refresh = () => {
         let score = 0;
         let total = 0;
-        document.querySelectorAll('.scorehover').forEach((a) => { score += Number(a.innerHTML.split('<')[0]); total += 10 });
-        document.querySelectorAll('.overall-grade-text')[0].innerHTML = String(((score / total) * 100).toFixed(2)) + "%";
+        document.querySelectorAll('.scorehover').forEach((a) => { score += Number(a?.innerHTML.split('<')[0]); total += 10 });
+        const gradeText = document.querySelectorAll('.overall-grade-text')[0];
+        if(gradeText) {
+            gradeText.innerHTML = String(((score / total) * 100).toFixed(2)) + "%";
+        }
 
-//         const scorecards = document.querySelectorAll('[title="View scores/feedback"]');
-//         scorecards.forEach((a, idx) => {
-//             a.style.display = 'block';
-//             a.style.textAlign = 'center';
-//             document.querySelectorAll('th')[idx].innerHTML.split('\n')[0];
-//             if(scorecards.length - config.lastCannotRA <= idx) return;
-//             let inner = document.createElement('a');
-//             inner.innerHTML = 'RA';
-//             inner.href = `${config.form.url}?entry.${config.form.fields.name}=${document.querySelectorAll('.scorehead')[0].innerHTML.split('>')[2].split('<')[0]}&entry.${config.form.fields.objective}=${document.querySelectorAll('th')[idx].innerHTML.split('\n')[0]}`;
-//             a.appendChild(inner);
-//         });
-    }, 2150);
+        // const scorecards = document.querySelectorAll('[title="View scores/feedback"]');
+        // scorecards.forEach((a, idx) => {
+        //     a.style.display = 'block';
+        //     a.style.textAlign = 'center';
+        //     document.querySelectorAll('th')[idx].innerHTML.split('\n')[0];
+        //     if(scorecards.length - config.lastCannotRA <= idx) return;
+        //     let inner = document.createElement('a');
+        //     inner.innerHTML = 'RA';
+        //     inner.href = `${config.form.url}?entry.${config.form.fields.name}=${document.querySelectorAll('.scorehead')[0].innerHTML.split('>')[2].split('<')[0]}&entry.${config.form.fields.objective}=${document.querySelectorAll('th')[idx].innerHTML.split('\n')[0]}`;
+        //     a.appendChild(inner);
+        // });
+
+        setTimeout(refresh, 1000);
+    }
+    refresh();
 }
